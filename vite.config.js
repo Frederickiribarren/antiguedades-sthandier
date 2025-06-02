096eda0256a183806633cabc2ec078309608dc4d
Frederick Iribarren <104109101+Frederickiribarren@users.noreply.github.com>
2025-06-02 16:23:05 -0400

    se crean modificaciones en vite.config app.jsx

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/antiguedades-sthandier/',
-});
+  base: '/antiguedades-sthandier/', // Cambia 'proyecto' por el nombre de tu repo si es diferente
+})
